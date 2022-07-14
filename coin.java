b0f34a44a2412d4de93bbfa10ab762750c808191
s1290173 <s1290173@u-aizu.ac.jp>
2022-07-14 19:06:12 +0900

    Victory message added

--- coin.java
+++ coin.java
@@ -1,29 +1,31 @@
-class coin{
 
+import java.util.Scanner;
+
+class coin{
     public static void main(String[] args){
 	int countH=0;
-	int countT=0;
+ 	int countT=0;
 
-	System.out.println("Tossing a coin...");
+ 	System.out.println("Tossing a coin...");
     
-	for(int i=1;i<=3;i++){
-	    int coin = new java.util.Random().nextInt(2);
-	    if(coin==0){
-		System.out.println("Round " +i+": Heads");
-		countH++;
-	    }
+ 	for(int i=1;i<=3;i++){
+ 	    int coin = new java.util.Random().nextInt(2);
+ 	    if(coin==0){
+ 		System.out.println("Round " +i+": Heads");
+ 		countH++;
+ 	    }
 	    else{
-		System.out.println("Round " +i+": Tails");
+ 		System.out.println("Round " +i+": Tails");
 		countT++;
-	    }
+ 	    }
+ 	}
+ 	System.out.println("Heads: "+countH+", Tails: "+countT);
+
+	if(countH >countT){
+	    System.out.println("John won!");
 	}
-	System.out.println("Heads: "+countH+", Tails: "+countT);
-    }
-    if(countH >countT){
-	System.out.println("John won!");
-    }
-    else{
-	System.out.println("John lost!");
+	else{
+	    System.out.println("John lost!");
+	}
     }
 }
-
