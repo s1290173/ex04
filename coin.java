3632406382e94b4ec8311cfaa32f3af19661c6c5
s1290173 <s1290173@u-aizu.ac.jp>
2022-07-14 18:43:26 +0900

    rm

--- coin.java
+++ coin.java
@@ -1,23 +1,23 @@
 class coin{
 
     public static void main(String[] args){
-	int countH=0;
-	int countT=0;
+ 	int countH=0;
+ 	int countT=0;
 
-	System.out.println("Tossing a coin...");
+ 	System.out.println("Tossing a coin...");
     
-	for(int i=1;i<=3;i++){
-	    int coin = new java.util.Random().nextInt(2);
-	    if(coin==0){
-		System.out.println("Round " +i+": Heads");
-		countH++;
-	    }
+ 	for(int i=1;i<=3;i++){
+ 	    int coin = new java.util.Random().nextInt(2);
+ 	    if(coin==0){
+ 		System.out.println("Round " +i+": Heads");
+ 		countH++;
+ 	    }
 	    else{
-		System.out.println("Round " +i+": Tails");
+ 		System.out.println("Round " +i+": Tails");
 		countT++;
-	    }
-	}
-	System.out.println("Heads: "+countH+", Tails: "+countT);
+ 	    }
+ 	}
+ 	System.out.println("Heads: "+countH+", Tails: "+countT);
     }	
 }
 
