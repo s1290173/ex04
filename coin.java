07562d6b77a289b6708172fe933209e635ea39d2
s1290173 <s1290173@u-aizu.ac.jp>
2022-07-14 23:37:33 +0900

    Victory message added

--- coin.java
+++ coin.java
@@ -1,8 +1,14 @@
-
 import java.util.Scanner;
 
 class coin{
     public static void main(String[] args){
+	String s;
+	System.out.println("Who are you?");
+	 
+	Scanner sc = new Scanner(System.in);
+	s = sc.next();
+	System.out.println("Hello, "+s+"!");
+	
 	int countH=0;
  	int countT=0;
 
